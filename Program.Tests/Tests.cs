062caa88cfdea84479ff19348e5809baa2facef2
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-11-06 03:04:55 +0200

    initial commit

---
+++ Program.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Program.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            double a = 2.0;
+            double b = 3.0;
+            double c = 1.0;
+            double expected = (Math.Sin(a * b * c) * Math.Sin(a * b * c) / (a * a + b * b + c * c));
+
+            double actual = Program.F(a, b, c);
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double s, t;
+            Console.Write("s = ");
+            s = double.Parse(Console.ReadLine());
+            Console.Write("t = ");
+            t = double.Parse(Console.ReadLine());
+
+            double c = (F(1, t * t, s) + F(t, s * s, 1)) / (1 + Math.Pow(F(1, t * s, 1), 2));
+
+            Console.WriteLine("c = " + c);
+        }
+
+        public static double F(double a, double b, double c)
+        {
+            return (Math.Sin(a * b * c) * Math.Sin(a * b * c) / (a * a + b * b + c * c));
+        }
+    }
+}
